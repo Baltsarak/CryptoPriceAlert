2067d50cf1d62e1998c2614d12dc951e62b96325
Baltsarak <evgeniy.bk.89@gmail.com>
2024-02-12 19:48:17 +0300

    Creating ApiService and ApiFactory

--- app/src/main/java/com/baltsarak/cryptopricealert/presentation/MainActivity.kt
+++ app/src/main/java/com/baltsarak/cryptopricealert/presentation/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.baltsarak.cryptopricealert.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.baltsarak.cryptopricealert.R
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.baltsarak.cryptopricealert.data.network.ApiFactory
+import com.baltsarak.cryptopricealert.databinding.ActivityMainBinding
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val compositeDisposable = CompositeDisposable()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        val disposable = ApiFactory.apiService.getFullPriceList(fSyms = "BTC,ETH,LTC")
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                Log.d("TEST_OF_LOADING_DATA", it.toString())
+            }, {
+                it.message?.let { it1 -> Log.d("TEST_OF_LOADING_DATA", it1) }
+            })
+        compositeDisposable.add(disposable)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.dispose()
     }
 }
