a628b50ce664dabe4e387c5556d338770ced265e
Baltsarak <evgeniy.bk.89@gmail.com>
2024-04-11 19:32:21 +0300

    creating a registration screen layout

---
+++ app/src/main/java/com/baltsarak/cryptopricealert/RegistrationActivity.kt
@@ -0,0 +1,11 @@
+package com.baltsarak.cryptopricealert
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
