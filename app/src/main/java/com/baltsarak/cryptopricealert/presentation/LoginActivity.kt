b22e478bbd6ba98d5a0810f0490bee7b8caca25d
Baltsarak <evgeniy.bk.89@gmail.com>
2024-04-12 12:30:49 +0300

    creating login screen layout

---
+++ app/src/main/java/com/baltsarak/cryptopricealert/presentation/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.baltsarak.cryptopricealert.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.baltsarak.cryptopricealert.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
